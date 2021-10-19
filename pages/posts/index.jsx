616f5327d4b032003fb250579b03bdcc7b2fdcdf
Mahesh Vagicherla <maheshvagicherla99438@gmail.com>
2021-10-19 23:29:53 +0530

    check

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-
-function Posts() {
+import React, { useEffect } from "react";
+import { promises as fs } from "fs";
+function Posts({ data }) {
+  useEffect(() => {
+    console.log(data);
+  });
   return (
     <div>
       <p>posts</p>
     </div>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const files = await fs.readdir(process.cwd());
+
+  return {
+    props: {
+      data: files,
+    },
+  };
 }
 
 export default Posts;
