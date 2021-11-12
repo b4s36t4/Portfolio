4919e8f137bc7ca57e4d031c7c07424d96c1143c
Mahesh Vagicherla <maheshvagicherla99438@gmail.com>
2021-11-12 22:19:37 +0530

    with caddy

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ import { promises as fs } from "fs";
 import matter from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
-import { cloudflareLoader } from "../utils/loader";
 
 const AboutMeDescription = () => {
   return (
@@ -66,7 +65,6 @@ const AboutMeAvatar = () => {
       width={280}
       height={280}
       quality={90}
-      loader={cloudflareLoader}
     />
   );
 };
